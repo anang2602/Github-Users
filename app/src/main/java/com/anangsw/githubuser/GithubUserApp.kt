3d922e06453e33083f573440282bf07ce33782e1
anang.suwasto <anang.suwasto@aryanoble.co.id>
2022-07-04 14:20:27 +0700

    support dark mode and light mode

--- app/src/main/java/com/anangsw/githubuser/GithubUserApp.kt
+++ app/src/main/java/com/anangsw/githubuser/GithubUserApp.kt
@@ -2,10 +2,16 @@ package com.anangsw.githubuser
 
 
 import com.anangsw.githubuser.di.DaggerApplicationComponent
+import com.google.android.material.color.DynamicColors
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class GithubUserApp: DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+//        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.factory().create(this)
